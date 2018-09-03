0ce0857b41cced22ee95bf55321f63376490f477
Violet Moon <violet.wmoon@gmail.com>
2018-09-03 04:25:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
-
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount
+  attr_reader :title, :price
+  
+@@total = 0
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    new_item = @total += price * quantity
+    @@total += new_item
+  end
+  
+  def apply_discount
+    discount = (@discount.to_f/100)
+    binding.pry
+    @total = @total - (@total * discount).to_i
+    return ""
+  end
+end
